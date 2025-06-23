c8dd2c1e08972c229edf44ee817d03345f8d408d
Randolph <randop@me.com>
2025-06-23 11:36:46 +0000

    + Write github action script for compiling mongo c driver

---
+++ scripts/mongoc.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+mkdir -p /opt/mongo-c-driver/current
+git clone -b v2.0.2 --depth 1 https://github.com/mongodb/mongo-c-driver.git /opt/mongo-c-driver/2.0.2
+cd /opt/mongo-c-driver/2.0.2 && cmake -DCMAKE_INSTALL_PREFIX=/opt/mongo-c-driver/current .
+cd /opt/mongo-c-driver/2.0.2 && make all install
+echo "/opt/mongo-c-driver/current" > /etc/ld.so.conf.d/boost.conf
+ldconfig
