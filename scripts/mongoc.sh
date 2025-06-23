b3f381c0c1ea715b2692e6b7f1f9abecced53cb0
Randolph <randop@me.com>
2025-06-23 12:48:04 +0000

    + Improve CI workflow

--- scripts/mongoc.sh
+++ scripts/mongoc.sh
@@ -3,7 +3,6 @@
 # IMPORTANT: Github ubuntu runner uses proprietary azure that has poor limitations and restrictions
 # Refer at https://docs.github.com/en/actions/using-github-hosted-runners/using-github-hosted-runners
 
-sudo apt install -y git meson libboost-all-dev
 sudo mkdir -p /opt/mongo-c-driver/current
 sudo git clone -b 2.0.2 --depth 1 https://github.com/mongodb/mongo-c-driver.git /opt/mongo-c-driver/2.0.2
 cd /opt/mongo-c-driver/2.0.2
