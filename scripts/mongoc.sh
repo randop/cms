7ed5d42fa8578b6a2df658d0988169ed4f570ebf
Randolph <randop@me.com>
2025-06-23 12:46:05 +0000

    + Fix CI mongo c compile process

--- scripts/mongoc.sh
+++ scripts/mongoc.sh
@@ -4,13 +4,8 @@
 # Refer at https://docs.github.com/en/actions/using-github-hosted-runners/using-github-hosted-runners
 
 sudo apt install -y git meson libboost-all-dev
-mkdir -p /tmp/mongo-c-driver/current
-git clone -b 2.0.2 --depth 1 https://github.com/mongodb/mongo-c-driver.git /tmp/mongo-c-driver/2.0.2
-cd /tmp/mongo-c-driver/2.0.2 && cmake -DCMAKE_INSTALL_PREFIX=/tmp/mongo-c-driver/current .
-cd /tmp/mongo-c-driver/2.0.2 && make all install
-
-export LD_LIBRARY_PATH=/usr/local/lib:/tmp/mongo-c-driver/current/lib
-
-echo "/tmp/mongo-c-driver/current" | sudo tee -a /etc/ld.so.conf.d/mongo.conf > /dev/null
-
-sudo ldconfig
+sudo mkdir -p /opt/mongo-c-driver/current
+sudo git clone -b 2.0.2 --depth 1 https://github.com/mongodb/mongo-c-driver.git /opt/mongo-c-driver/2.0.2
+cd /opt/mongo-c-driver/2.0.2
+sudo cmake -DCMAKE_INSTALL_PREFIX=/opt/mongo-c-driver/current .
+sudo make all install
