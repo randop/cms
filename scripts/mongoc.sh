dcb479543bc3c2a40afc5187c4aacb9bdc46baee
Randolph <randop@me.com>
2025-06-23 11:59:54 +0000

    + Update CI script

--- scripts/mongoc.sh
+++ scripts/mongoc.sh
@@ -1,6 +1,15 @@
 #!/usr/bin/env bash
+
+# IMPORTANT: Github ubuntu runner uses proprietary azure that has poor limitations and restrictions
+# Refer at https://docs.github.com/en/actions/using-github-hosted-runners/using-github-hosted-runners
+
 sudo apt install -y git meson
 mkdir -p /tmp/mongo-c-driver/current
 git clone -b 2.0.2 --depth 1 https://github.com/mongodb/mongo-c-driver.git /tmp/mongo-c-driver/2.0.2
 cd /tmp/mongo-c-driver/2.0.2 && cmake -DCMAKE_INSTALL_PREFIX=/tmp/mongo-c-driver/current .
 cd /tmp/mongo-c-driver/2.0.2 && make all install
+
+export LD_LIBRARY_PATH=/usr/local/lib:/tmp/mongo-c-driver/current/lib
+
+echo "Configuring build..." && meson setup build --prefer-static --default-library=static
+echo "Compiling application..." && meson compile -C build
