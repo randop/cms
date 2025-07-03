255e34020720c0551987d40c817ffc570ff12cbd
Randolph Ledesma <randop@me.com>
2025-07-03 13:47:44 +0000

    + Disable OCSP to fix segmentation fault caused by mongo c driver

--- scripts/mongoc.sh
+++ scripts/mongoc.sh
@@ -5,6 +5,7 @@
 
 sudo mkdir -p /opt/mongo-c-driver/current
 sudo git clone -b 2.0.2 --depth 1 https://github.com/mongodb/mongo-c-driver.git /opt/mongo-c-driver/2.0.2
+sudo sed -i "/#if (OPENSSL_VERSION_NUMBER >= 0x10001000L)/i #define OPENSSL_NO_OCSP 1" /opt/mongo-c-driver/2.0.2/src/libmongoc/src/mongoc/mongoc-openssl-private.h
 cd /opt/mongo-c-driver/2.0.2
 sudo cmake -DCMAKE_INSTALL_PREFIX=/opt/mongo-c-driver/current .
 sudo make all install
