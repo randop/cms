4ae1cf34e8c36f230adadd49f5e4f7d97a4449eb
Randolph Ledesma <randopledesma+gitlab@hotmail.com>
2026-08-31 04:05:07 +0000

    ci(fix): repair mongo c and c++ driver script
    ./scripts/mongocpp.sh: line 34: /etc/ld.so.conf.d/mongocpp-driver.conf:
    Permission denied
    Error: Process completed with exit code 1.

--- scripts/mongoc.sh
+++ scripts/mongoc.sh
@@ -21,7 +21,7 @@ if [ ! -d "/opt/mongo-c-driver/${MONGODBCDRIVER_VERSION}" ]; then
     -DENABLE_TESTS=OFF \
     -DENABLE_EXAMPLES=OFF
   sudo make -j$(nproc) && make install
-  sudo echo "/opt/mongo-c-driver/current/lib" >/etc/ld.so.conf.d/mongoc-driver.conf
+  sudo echo "/opt/mongo-c-driver/current/lib" | sudo tee /etc/ld.so.conf.d/mongoc-driver.conf
   sudo ldconfig
   sudo rm -fv /usr/lib/pkgconfig/mongoc.pc /usr/lib/pkgconfig/bson.pc
   sudo rm -fv /usr/lib/pkgconfig/mongoc-static.pc /usr/lib/pkgconfig/bson-static.pc
