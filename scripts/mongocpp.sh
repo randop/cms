922b431540a1b4e240fada4b2dc86262781862b8
Randolph Ledesma <randopledesma+gitlab@hotmail.com>
2026-08-31 03:31:31 +0000

    ci(fix): update mongo c++ driver script to fix github actions issue
    Run ./scripts/mongocpp.sh
      ./scripts/mongocpp.sh
      shell: /usr/bin/bash -e {0}
    ./scripts/mongocpp.sh: line 6: CMAKE_PREFIX_PATH: unbound variable
    Error: Process completed with exit code 1.

--- scripts/mongocpp.sh
+++ scripts/mongocpp.sh
@@ -3,7 +3,7 @@
 set -euo pipefail
 
 export MONGODBCPPDRIVER_VERSION="4.5.1"
-export CMAKE_PREFIX_PATH="/opt/mongo-c-driver/current/lib/cmake:$CMAKE_PREFIX_PATH"
+export CMAKE_PREFIX_PATH="/opt/mongo-c-driver/current/lib/cmake"
 MONGODBCPP_DIR="/opt/mongo-cpp-driver"
 MONGODBCPP_CURRENT="${MONGODBCPP_DIR}/current"
 MONGODBCPP_VERSION_DIR="${MONGODBCPP_DIR}/${MONGODBCPPDRIVER_VERSION}"
